e1b9773036819b274605dbf735c45a224b75cc54
Maliur <zene@tuta.io>
2020-09-17 22:18:23 +0200

    Remove italics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.1.1 - 2020-09-17
+
+- Remove Italics
+
 ## 0.1.0 - 2020-09-16
 
 - Initial release
--- src/syntax.js
+++ src/syntax.js
@@ -14,9 +14,9 @@ function createScope(scope, foreground, fontStyle) {
 
 export default function syntax(theme) {
 	return Object.assign([
-		createScope(prog.accessors, theme.orange, 'italic'),
+		createScope(prog.accessors, theme.orange, 'regular'),
 		createScope(prog.classes, theme.greenalt),
-		createScope(prog.comments, `${theme.primary}80`, 'italic'),
+		createScope(prog.comments, `${theme.primary}80`, 'regular'),
 		createScope(prog.constants, theme.red),
 		createScope(prog.currentText, theme.default),
 		createScope(prog.decorators, theme.pink),
@@ -28,8 +28,8 @@ export default function syntax(theme) {
 		createScope(prog.dataProperty, theme.yellow),
 		createScope(prog.operators, theme.yellow),
 		createScope(prog.punctuation, `${theme.default}aa`),
-		createScope(prog.self, theme.orange, 'italic'),
-		createScope(prog.storages, theme.turquoize, 'italic'),
+		createScope(prog.self, theme.orange, 'regular'),
+		createScope(prog.storages, theme.turquoize, 'regular'),
 		createScope(prog.strings, theme.green),
 		createScope(prog.types, theme.purple),
 		createScope(prog.variables, theme.salmon),
@@ -39,7 +39,7 @@ export default function syntax(theme) {
 		createScope(markup.variables, theme.salmon),
 		createScope(markup.tags, theme.blue),
 		createScope(markup.tagsPunctuation, `${theme.blue}aa`),
-		createScope(markup.attributes, theme.yellow, 'italic'),
+		createScope(markup.attributes, theme.yellow, 'regular'),
 		// MARKDOWN
 		createScope(markup.mdCode, theme.purple),
 		createScope(markup.mdCodeLanguage, theme.purple),
@@ -49,20 +49,20 @@ export default function syntax(theme) {
 		createScope(markup.mdHeadingPunctuation, theme.yellow),
 		createScope(markup.mdLink, theme.blue),
 		createScope(markup.mdBold, theme.blue, 'bold'),
-		createScope(markup.mdItalic, theme.blue, 'italic'),
-		createScope(markup.mdQuote, theme.blue, 'italic'),
-		createScope(markup.mdQuotePunctuation, theme.blue, 'italic'),
+		createScope(markup.mdregular, theme.blue, 'regular'),
+		createScope(markup.mdQuote, theme.blue, 'regular'),
+		createScope(markup.mdQuotePunctuation, theme.blue, 'regular'),
 
 		// STYLING
 		createScope(styling.pseudoClasses, theme.pink),
 		createScope(styling.pseudoElements, theme.pink),
-		createScope(styling.classes, theme.turquoize, 'italic'),
-		createScope(styling.suffix, theme.turquoize, 'italic'),
+		createScope(styling.classes, theme.turquoize, 'regular'),
+		createScope(styling.suffix, theme.turquoize, 'regular'),
 		createScope(styling.property, theme.default),
 		createScope(styling.ids, theme.purple),
 		createScope(styling.and, theme.orange),
 		// createScope(styling.punctuation, theme.default),
-		createScope(styling.units, theme.orange, 'italic'),
+		createScope(styling.units, theme.orange, 'regular'),
 		createScope(styling.value, theme.orange),
 		createScope(styling.nums, theme.orange),
 	]);
